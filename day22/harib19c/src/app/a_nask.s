015c8087c87e81edffbb6f47d765b38ac81c69d1
youliangdao <youliangdaojing5@gmail.com>
2022-03-15 23:29:43 +0900

    例外を起こした番地を調べれるようにする

---
+++ day22/harib19c/src/app/a_nask.s
@@ -0,0 +1,14 @@
+.file "a_nask.s"
+.arch i486
+
+        .global api_putchar, api_end
+.text
+api_putchar:    # void api_putchar(int c)
+        movl $1, %edx
+        movb 4(%esp), %al
+        int $0x40
+        ret
+
+api_end:        # void api_end(void)
+        movl $4, %edx
+        int $0x40
