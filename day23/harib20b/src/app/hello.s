f3269c91fba056ed6d8ecacd0a38702628f9fca0
youliangdao <youliangdaojing5@gmail.com>
2022-03-17 23:42:47 +0900

    点を描く

---
+++ day23/harib20b/src/app/hello.s
@@ -0,0 +1,17 @@
+.arch i486
+
+.text
+    movl    $msg, %ecx
+    movl    $1, %edx
+putloop:
+    movb    %cs:(%ecx), %al
+    cmpb    $0, %al
+    je      fin
+    int     $0x40
+    addl    $1, %ecx
+    jmp     putloop
+fin:
+    movl    $4, %edx
+    int     $0x40
+msg:
+    .string "hello"
