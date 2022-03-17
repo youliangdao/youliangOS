cb5e664b39538c94787d394a27a4ead5623d1c07
youliangdao <youliangdaojing5@gmail.com>
2022-03-17 13:28:16 +0900

    C言語で文字列表示（１）

---
+++ day22/harib19d/src/app/a_nask.s
@@ -0,0 +1,23 @@
+.file "a_nask.s"
+.arch i486
+
+        .global api_putchar, api_end
+        .global api_putstr0
+.text
+api_putchar:    # void api_putchar(int c)
+        movl $1, %edx
+        movb 4(%esp), %al
+        int $0x40
+        ret
+
+api_end:        # void api_end(void)
+        movl $4, %edx
+        int $0x40
+
+api_putstr0:	# void api_putstr0(char *s);
+	pushl	%ebx
+	movl	$2, %edx
+	movl	8(%esp), %ebx	# s
+	int		$0x40
+	popl	%ebx
+	ret
