9031991a2e3b179fdb1cf69dee8a7c1ec3e82749
youliangdao <youliangdaojing5@gmail.com>
2022-03-17 16:19:25 +0900

    ウィンドウを出そう

---
+++ day22/harib19f/src/app/a_nask.s
@@ -0,0 +1,41 @@
+.file "a_nask.s"
+.arch i486
+
+        .global api_putchar, api_end
+        .global api_putstr0
+        .global api_openwin
+.text
+api_putchar:    # void api_putchar(int c)
+        movl $1, %edx
+        movb 4(%esp), %al
+        int $0x40
+        ret
+
+api_end:        # void api_end(void)
+        movl $4, %edx
+        int $0x40
+
+api_putstr0:	# void api_putstr0(char *s);
+	pushl	%ebx
+	movl	$2, %edx
+	movl	8(%esp), %ebx	# s
+	int		$0x40
+	popl	%ebx
+	ret
+
+api_openwin:    # int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
+        pushl   %edi
+        pushl   %esi
+        pushl   %ebx
+        movl    $5, %edx
+        movl    16(%esp), %ebx  # buf
+        movl    20(%esp), %esi  # xsiz
+        movl    24(%esp), %edi  # ysiz
+        movl    28(%esp), %eax  # col_inv
+        movl    32(%esp), %ecx  # title
+        int     $0x40
+
+        popl    %ebx
+        popl    %esi
+        popl    %edi
+        ret
